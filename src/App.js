80c306487f73377515627261c7a9db810069479e
huahaoworkspace@163.com <huahaoworkspace@163.com>
2019-06-01 14:07:38 +0800

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from './redux/index.js';
-import {CSSTransition } from "react-transition-group";
+import {CSSTransition,TransitionGroup} from "react-transition-group";
 import Login from './views/Login/login';
 import Index from './views/Index';
 import PrivateRoute from './router/PrivateRoute';
@@ -13,18 +13,21 @@ function App() {
   return (
     <Provider store={store}>
       <Router basename="/echo">
-        <div className="entryWrap">
-          <CSSTransition
-            classNames="fade"
-            timeout={1000}
-          >
-            <Switch>
-              <Route path="/login" component={Login} exact/>
-              <PrivateRoute path="/" component={Index}/>
-              <Route component={NoMatch}/>
-            </Switch>
-          </CSSTransition>
-        </div>
+        <TransitionGroup>
+          <div className="entryWrap">
+
+              <CSSTransition
+                classNames="fade"
+                timeout={1000}
+              >
+                <Switch>
+                  <Route path="/login" component={Login} exact/>
+                  <PrivateRoute path="/" component={Index}/>
+                  <Route component={NoMatch}/>
+                </Switch>
+              </CSSTransition>
+          </div>
+        </TransitionGroup>
       </Router>
     </Provider>
   );
