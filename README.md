ad8b8a89d4d4151c580a6b4f2f71583932a0c85d
huahaoworkspace@163.com <huahaoworkspace@163.com>
2019-06-01 14:16:37 +0800

    add screenshot

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
 ## React-Admin-System
 
-##### 开发版本依赖：
->
-antd: 3.19.1,
-axios: 0.19.0,
-react: 16.8.6,
-react-dom: 16.8.6,
-react-redux: 7.0.3,
-react-router-dom: 5.0.0,
-react-transition-group: 4.1.0,
-redux: 4.0.1
-
+##### 开发环境：
--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from './redux/index.js';
-import {CSSTransition,TransitionGroup} from "react-transition-group";
+import {CSSTransition} from "react-transition-group";
 import Login from './views/Login/login';
 import Index from './views/Index';
 import PrivateRoute from './router/PrivateRoute';
@@ -13,7 +13,6 @@ function App() {
   return (
     <Provider store={store}>
       <Router basename="/echo">
-        <TransitionGroup>
           <div className="entryWrap">
 
               <CSSTransition
@@ -27,7 +26,6 @@ function App() {
                 </Switch>
               </CSSTransition>
           </div>
-        </TransitionGroup>
       </Router>
     </Provider>
   );
