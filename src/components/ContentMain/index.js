96ba07a7f975bea84a2e34a69f17544785bd6758
huahaoworkspace@163.com <huahaoworkspace@163.com>
2019-06-06 09:54:12 +0800

    fixed the contentMain

--- src/components/ContentMain/index.js
+++ src/components/ContentMain/index.js
@@ -1,15 +1,7 @@
 import React,{Component} from 'react';
 import {Switch,withRouter,Route} from 'react-router-dom';
 import PrivateRoute from '../../router/PrivateRoute';
-import DataChart from '../../views/DataChart';
-import Basic from '../../views/Basic';
-import Form from '../../views/Form';
-import Message from '../../views/Message';
-import Alert from '../../views/Alert';
-import Spin from '../../views/Spin';
-import Progress from '../../views/Progress';
-import Checkbox from '../../views/Checkbox';
-import Cascader from '../../views/Cascader';
+import {routes} from '../../router/route';
 import NoMatch from '../../views/NoMatch';
 import './index.css';
 class ContentMain extends Component{
@@ -17,16 +9,14 @@ class ContentMain extends Component{
         return(
             <div className="routeWrap">
                 <Switch>
-                    <PrivateRoute path="/dataCount" exact component = {DataChart}/>
-                    <PrivateRoute path="/basic" exact component = {Basic}/>
-                    <PrivateRoute path="/form" exact component = {Form}/>
-                    <PrivateRoute path="/alert" exact component = {Alert}/>
-                    <PrivateRoute path="/message" exact component = {Message}/>
-                    <PrivateRoute path="/spin" exact component = {Spin}/>
-                    <PrivateRoute path="/progress" exact component = {Progress}/>
-                    <PrivateRoute path="/checkbox" exact component = {Checkbox}/>
-                    <PrivateRoute path="/cascader" exact component = {Cascader}/>
-                    <Route component={NoMatch}/>
+                    {
+                        routes.map(item=>{
+                            return (
+                                item.path?<PrivateRoute path={item.path} exact component={item.component}/>:<Route component={NoMatch}/>
+                            )
+                        })
+                    }
+                    
                 </Switch>
             </div>
         )
