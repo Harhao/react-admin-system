65d30b8895b72322ada38eeb01d5646a9e9f0c35
huahaoworkspace@163.com <huahaoworkspace@163.com>
2019-06-06 11:37:06 +0800

    fixed PrivateRoute.js

--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -3,18 +3,20 @@ import {Route,Redirect} from 'react-router-dom';
 import {fakeAuth} from '../util/fakeAuth';
 import {connect} from 'react-redux';
 const PrivateRoute = ({component:Component,...rest})=>{
-        return (
-            <Route
-                {...rest}
-                render = {props => (fakeAuth.authenticate())?(<Component {...props}/>):(
-                    <Redirect to={{
-                        pathname:"/login",
-                        state:{from:props.location}
-                    }}/>
-                )}
-            ></Route>
-        )
-    
+        if(fakeAuth.authenticate() && !rest.isTokenTimeout){
+            return (
+                <Route
+                    {...rest}
+                    render = {props => (<Component {...props}/>)}
+                ></Route>
+            )
+        }else{
+            return (
+                <Redirect to={{
+                    pathname:"/login",
+                }}/>
+            )
+        } 
 }
 function mapStateToProps(state){
     console.log(state);
